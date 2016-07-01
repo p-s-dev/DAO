36ea126947af1f8c4785d87c0f94793423f57c39
p-s-dev <p+psdev@psdev.io>
2016-07-01 08:39:30 -0700

    no address

--- mkdeploy.sh
+++ mkdeploy.sh
@@ -16,3 +16,6 @@ echo "var creatorContract = web3.eth.contract(creator_abi);" >> dodeploy.js
 echo "var daoCreatorContract = creatorContract.new({from: web3.eth.accounts[0], data: creator_bin, gas: 4000000});" >> dodeploy.js
 
 
+echo " var daoContract = web3.eth.contract(dao_abi); " >> dodeploy.js
+
+echo " var dao = daoContract.new('', '', web3.toWei(0, 'ether'), web3.toWei(1, 'ether'), 1467344365, 0, {from: eth.accounts[0], data: dao_bin, gas: 4000000}); " >>dodeploy.js
